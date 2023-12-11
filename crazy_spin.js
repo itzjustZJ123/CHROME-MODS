e866c1dd41774f854a1258944ca4d5c969f9b7ce
itzjustZJ123 <91702939+itzjustZJ123@users.noreply.github.com>
2023-12-11 12:29:22 -0500

    Delete crazy_spin.js

--- crazy_spin.js
+++
@@ -1,1 +0,0 @@
-javascript:(function(){document.documentElement.style.transitionDuration="60s";document.documentElement.style.transitionTimingFunction="ease-in";document.documentElement.style.transform="rotate(360000deg)";}());
